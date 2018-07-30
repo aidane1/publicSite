a335610d0d5ab8b14b814397a062deb9e4630500
aidan <aidaneglin@gmail.com>
2018-07-29 18:05:18 -0700

    updated requirements

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 let express = require("express");
 
+let mongoose = require("mongoose");
+
+let fs = require("fs");
+
+let bodyParser = require("body-parser");
+
+let User = require("../models/userchar");
+
+let Course = require("../models/coursechar");
+
+let session = require("express-session");
+
+let Events = require("../models/eventschar");
+
+let socket = require("socket.io")
+
+let cookieParser = require("cookie-parser");
+
+let Texts = require("../models/textchar.js");
+
+let Resources = require("../models/resourcechar.js");
+
+let nodemailer = require("nodemailer");
+
 
 let app = express();
 
