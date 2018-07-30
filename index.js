7d5d81742a73c46a792cf27a52a86d565124f6d6
aidan <aidaneglin@gmail.com>
2018-07-29 17:49:02 -0700

    syntax error and socket overlap

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let express = require("express");
 
 
-let app = exress();
+let app = express();
 
-app.listen(3000, function() {
+app.listen(5000, function() {
   console.log("listening for requests");
 });
 
