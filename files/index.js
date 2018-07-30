c5f5070c6f810b0b4e9298863bc5eb11e8803b6f
aidan <aidaneglin@gmail.com>
2018-07-29 18:33:46 -0700

    added signup

--- files/index.js
+++ files/index.js
@@ -33,7 +33,7 @@ mongoose.connection.once("open", function() {
   console.log("connection error: " + error);
 });
 
-
+let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 let app = express();
 
@@ -50,7 +50,7 @@ app.use(express.static('public'));
 app.use(cookieParser());
 
 
-var server = app.listen(4000, function() {
+let server = app.listen(4000, function() {
   console.log("listening for requests");
 });
 
@@ -58,6 +58,62 @@ app.get("/", function(req,res) {
   res.redirect("/login");
 });
 
+
+
+
+
 app.get("/login", function(req, res) {
   res.render("login", {error: ""})
 })
+
+app.post("/login", urlencodedParser, async (req, res, next) => {
+  try {
+    let response = await User.authenticate(req.body.logname, req.body.logword);
+    req.session.userId = response._id;
+    res.cookie("sessionID", response._id);
+    res.redirect('/');
+  } catch(e) {
+    console.log(e);
+    res.render("login", {error : "Username or Password incorrect. Please try again."});
+  }
+});
+
+
+
+
+app.get("/signup", function(req, res) {
+  res.render("signup", {error : "", data : ["", "", "", ""]});
+});
+
+
+app.post("/signup", urlencodedParser, function(req, res) {
+  if (req.body.password != req.body.passwordConf) {
+    res.render("signup", {error : "Error : passwords do not match", data : [req.body.email, req.body.firstName, req.body.lastName]});
+  } else if (req.body.username && req.body.firstName && req.body.lastName && req.body.password && (req.body.password === req.body.passwordConf)) {
+    var userData = {
+      firstName: req.body.firstName.charAt(0).toUpperCase() + req.body.firstName.slice(1),
+      lastName: req.body.lastName.charAt(0).toUpperCase() + req.body.lastName.slice(1),
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.username
+    }
+    try {
+      User.create(userData,function(error, user) {
+        if (error) {
+          console.log(error);
+          res.render("signup", {error : "Username is already being used. Please try again.", data : ["", req.body.firstName, req.body.lastName]});
+        } else {
+          req.session.userId = user._id;
+          res.cookie("sessionID", req.session.userId);
+          return res.redirect("/courses");
+        }
+      });
+    } catch(e) {
+      console.log(e);
+    }
+
+
+  } else {
+    res.redirect("/signup");
+  }
+});
