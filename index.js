29faece90f47a1aa9e35e8efbaa34d430bc4b032
aidan <aidaneglin@gmail.com>
2018-07-29 17:47:37 -0700

    helloworld test

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 let express = require("express");
+
+
+let app = exress();
+
+app.listen(3000, function() {
+  console.log("listening for requests");
+});
+
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/index.html");
+});
