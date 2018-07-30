9fbdcef527f4a295b666e2dd14b6dbb635f34981
aidan <aidaneglin@gmail.com>
2018-07-30 14:14:07 -0700

    tooooo many redirects

--- files/index.js
+++ files/index.js
@@ -136,10 +136,14 @@ let server = app.listen(80, function() {
   console.log("listening for requests");
 });
 
-app.get("*", function(req, res) {
-  if (!req.secure) {
-      res.redirect("https://pvstudents.ca" + req.url);
-  }
+app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
 });
 
 
