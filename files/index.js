62ae33aad591085dc1ad014d5c3b954504c46602
aidan <aidaneglin@gmail.com>
2018-08-02 11:14:29 -0700

    added

--- files/index.js
+++ files/index.js
@@ -151,11 +151,7 @@ let server = app.listen(80, function() {
 
 
 app.get("/periodic-table", function(req, res) {
-  if (req.session.userId) {
-    res.sendFile(__dirname + "/public/html/periodicTable.html");
-  } else {
-    res.redirect("/login");
-  }
+  res.sendFile(__dirname + "/public/html/periodicTable.html");
 });
 
 
