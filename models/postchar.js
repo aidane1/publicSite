fdd077e8c9563cf790988191226df92d3320efa1
aidan <aidaneglin@gmail.com>
2018-08-03 16:43:20 -0700

    added

---
+++ models/postchar.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+
+
+const postSchema = new Schema({
+  _id : {
+    type: Schema.types.ObjectId
+  }
+  date: {
+    type: Date
+  },
+  title: {
+    type: String
+  },
+  submittedBy: {
+    type: String
+  },
+  comments: {
+    [{type: Schema.types.ObjectId, ref: }]
+  }
+});
+
+
+const commentSchema = new Schema({
+  parentPost: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+  },
+  body: {
+    type: String
+  }
+  submittedBy: {
+    type: String
+  }
+});
+
+var Post = mongoose.model('Post', postSchema);
+var Comment = mongoose.model("Comment", commentSchema);
+module.exports = {
+  Post,
+  Comment
+}
