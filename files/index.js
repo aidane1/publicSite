3e1136af9a3784afec724981b16e10c3c0ed9205
aidan <aidaneglin@gmail.com>
2018-08-08 11:58:32 -0700

    added

--- files/index.js
+++ files/index.js
@@ -223,8 +223,8 @@ app.get("/", function(req, res) {
                   }
                 }
               });
-              console.log((blockToTime((currentDate).getDay() -1)));
-              res.render("index", {courses: courses, homework: homeworkList, todaysCourses: blockToTime((currentDate).getDay() -1), blockOrder: todaysOrderedClasses, calendar: daysArray, month: months[currentDate.getMonth()], lcSchedule: lcSchedule(blockToTime((currentDate).getDay() -1)), permissions: user.permissions});
+              console.log((lcSchedule((currentDate).getDay() -1)));
+              res.render("index", {courses: courses, homework: homeworkList, todaysCourses: blockToTime((currentDate).getDay() -1), blockOrder: todaysOrderedClasses, calendar: daysArray, month: months[currentDate.getMonth()], lcSchedule: lcSchedule(((currentDate).getDay() -1)), permissions: user.permissions});
               // res.render("index",  {courses : user.courses, homework : homeworkList, todaysCourses : blockToTime(3), blockOrder : todaysOrderedClasses, calendar : daysArray, month: months[currentDate.getMonth()], lcSchedule : lcSchedule(3), permissions: user.permissions});
             });
           });
