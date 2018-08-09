74938643f42f3b171849950b338323513b89cd7c
aidan <aidaneglin@gmail.com>
2018-08-09 14:10:55 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -10,3 +10,8 @@
 self.addEventListener("install", e => {
   console.log("service worker: installed");
 });
+
+
+self.addEventListener("activated", e => {
+  console.log("service worker: activated");
+});
