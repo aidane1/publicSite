61f9d34970b09b097d54dfa94fc32d236d532dca
aidan <aidaneglin@gmail.com>
2018-08-09 14:28:22 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -38,7 +38,7 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   console.log("service worker: fetching");
-  console.log(Cache);
+  console.log(Caches);
   // console.log(e.request);
   // e.respondWith(
   //   caches.match(e.request)
