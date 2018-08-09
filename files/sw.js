ce421628fd642f4c4fb7b8a8a2cb51fa42c4e079
aidan <aidaneglin@gmail.com>
2018-08-08 21:48:56 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -21,15 +21,15 @@ self.addEventListener("activate", e => {
   }));
 });
 //
-// self.addEventListener("fetch", e => {
-//   console.log("service worker: fetching");
-//   e.respondWith(
-//     fetch(e.request).then(res => {
-//       const resClone = res.clone();
-//       caches.open(cacheName).then(cache => {
-//         cache.put(e.request, resClone);
-//       });
-//       return res;
-//     }).catch(err => caches.match(e.request).then(res => res));
-//   );
-// });
+self.addEventListener("fetch", e => {
+  console.log("service worker: fetching");
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const resClone = res.clone();
+      caches.open(cacheName).then(cache => {
+        cache.put(e.request, resClone);
+      });
+      return res;
+    }).catch(err => caches.match(e.request).then(res => res));
+  );
+});
