58fc558d3063348d9f12daa8dd568bf268e8dd35
aidan <aidaneglin@gmail.com>
2018-08-09 13:45:37 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -30,16 +30,14 @@ self.addEventListener("activate", e => {
 //         );
 //     })
 //   );
-// });
+});
 
 self.addEventListener("fetch", e => {
   console.log("service worker: fetching");
   e.respondWith(fetch(e.request).catch(() => {
       console.log(e.request);
-
       caches.match(e.request);
-    })
-  );
+    }));
 });
 
 
