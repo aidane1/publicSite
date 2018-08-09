97f5181f60dd886ef7dfe9abd0429315c7e0129d
aidan <aidaneglin@gmail.com>
2018-08-09 12:55:27 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -1,39 +1,61 @@
-'use strict';
 
-var cacheVersion = 1;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
+const cacheName = "v1";
+
+const cacheAssets = [
+  "public/html/offline.html"
+];
+
 const offlineUrl = 'public/html/offline.html';
 
-this.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([
-          offlineUrl
-      ]);
-    })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("service worker: caching files");
+      cache.addAll(cacheAssets);
+    }).then(() => self.skipWaiting());
   );
 });
 
-
-this.addEventListener('fetch', event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        // Respond with everything else if we can
-        event.respond With(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
+self.addEventListener("activate", e => {
+  console.log("service  worker: Activated");
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cache => {
+            if (cache !== cacheName) {
+              console.log("deleting old cache");
+              return caches.delete(cache);
+            }
+          });
+        );
+    });
+  );
 });
+
+self.addEventListener("fetch", e => {
+  console.log("service worker: fetching");
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
+
+
+
+// this.addEventListener('fetch', event => {
+//   // request.mode = navigate isn't supported in all browsers
+//   // so include a check for Accept: text/html header.
+//   if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+//         event.respondWith(
+//           fetch(event.request.url).catch(error => {
+//               // Return the offline page
+//               return caches.match(offlineUrl);
+//           })
+//     );
+//   }
+//   else{
+//         // Respond with everything else if we can
+//         event.respond With(caches.match(event.request)
+//                         .then(function (response) {
+//                         return response || fetch(event.request);
+//                     })
+//             );
+//       }
+// });
