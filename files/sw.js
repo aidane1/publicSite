f77ae448ffb95b4afe95b255ab6fe13f80ed5f36
aidan <aidaneglin@gmail.com>
2018-08-09 14:15:45 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -1,14 +1,21 @@
-//
-// const cacheName = "v2";
-//
-// const cacheAssets = [
-//   "public/html/offline.html"
-// ];
-//
-// const offlineUrl = 'public/html/offline.html';
+const cacheName = "v1";
+
+const cacheAssets = [
+  "public/html/offline.html"
+]
 
 self.addEventListener("install", e => {
   console.log("service worker: installed");
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("service worker: Caching files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
 
