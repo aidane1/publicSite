b1e947d69daa0039ada8807032d62198025119ed
aidan <aidaneglin@gmail.com>
2018-08-08 21:48:39 -0700

    added

--- files/sw.js
+++ files/sw.js
@@ -8,18 +8,18 @@ self.addEventListener("install", e => {
   console.log("service worker: installed");
 });
 //
-// self.addEventListener("activate", e => {
-//   console.log("service worker: activated");
-//   //remove unwanted caches
-//   e.waitUntil(caches.keys().then(cacheNames => {
-//     return Promise.all(cacheNames.map(cache => {
-//       if (cache !== cacheName) {
-//         console.log("deleting old cache");
-//         return caches.delete(cache);
-//       }
-//     }));
-//   }));
-// });
+self.addEventListener("activate", e => {
+  console.log("service worker: activated");
+  //remove unwanted caches
+  e.waitUntil(caches.keys().then(cacheNames => {
+    return Promise.all(cacheNames.map(cache => {
+      if (cache !== cacheName) {
+        console.log("deleting old cache");
+        return caches.delete(cache);
+      }
+    }));
+  }));
+});
 //
 // self.addEventListener("fetch", e => {
 //   console.log("service worker: fetching");
