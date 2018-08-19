4b05c018038959c6bb374259aa68c79ae02fdc78
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2018-08-18 20:00:30 -0700

    added

--- files/index.js
+++ files/index.js
@@ -165,16 +165,16 @@ app.use(cookieParser());
 app.enable('trust proxy');
 //
 //
-// app.use (function (req, res, next) {
-//   if (req.secure) {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.headers.host + req.url);
-//   }
-// });
+app.use (function (req, res, next) {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect('http://' + req.headers.host + req.url);
+  }
+});
 
 
-let server = app.listen(8080, function() {
+let server = app.listen(80, function() {
   console.log("listening for requests");
 });
 
