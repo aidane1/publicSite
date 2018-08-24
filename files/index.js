47a6f71ea2bc9ea32ed4f725c25844aef0cfcc55
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2018-08-24 10:06:24 -0700

    added a user authentification to access grade chatrooms

--- files/index.js
+++ files/index.js
@@ -287,9 +287,10 @@ app.get("/", async (req, res, next) => {
 
 
         let timeInMinutes = (currentDate.getHours()+7)*60 + currentDate.getMinutes();
-
+        console.log(timeInMinutes);
         timeInMinutes -= (9*60 + 10);
         let blockForTime = [];
+        console.log(timeInMinutes);
 
         if (timeInMinutes < 0) {
           blockForTime = [[0.5, ""], [0, "9:10-10:12 : "]];
@@ -311,7 +312,7 @@ app.get("/", async (req, res, next) => {
           blockForTime = [[3.5, ""], [3.5, ""]];
         }
 
-
+        console.log(blockForTime);
 
 
         res.render("index", {currentBlock: blockForTime, font: holidayFont(user.font), order: user.order, colours: user.colors, username: user.username, courses: courses, homework: homeworkList, todaysCourses: blockToTime((currentDate).getDay() -1), blockOrder: todaysOrderedClasses, calendar: daysArray, month: months[currentDate.getMonth()], lcSchedule: lcSchedule(((currentDate).getDay() -1)), permissions: user.permissions, soonEvents: soonEvents});
