abcfb658b729465d753ba09d477ddff617dd4790
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2018-10-07 21:41:47 -0700

    minor UI changes

--- files/index.js
+++ files/index.js
@@ -1525,7 +1525,16 @@ app.get("/weekly-schedule", function(req, res) {
   if (req.session.userId) {
     User.findOne({_id : req.session.userId}, function(err, user) {
       let userPlans = user.weeklySchedule;
-      let colours = ["orange", "blue", "red", "green", "purple"];
+      let colours = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
       res.render("weekschedule", {colourArray : colours, userName : user.username, weekPlans : userPlans, colours : user.colors, font : holidayFont(user.font)});
     });
 
@@ -1579,7 +1588,16 @@ app.get("/planner", function(req, res) {
             userPlans.push(user.planner[i][1]);
           }
         }
-        let colours = ["orange", "blue", "red", "green", "purple"];
+        let colours = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
         let monthCounter = 0;
         let currentMonth = 8;
         let currentYear = 2018;
