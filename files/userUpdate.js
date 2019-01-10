87cf5642bfd972ee94f693d0c2ddd640ebff293e
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-01-09 18:42:09 -0800

    user updater

--- files/userUpdate.js
+++ files/userUpdate.js
@@ -34,7 +34,7 @@ async function main() {
         let currentCourses = await Course.find({_id : users[i].courses});
         let newCourses = [];
         for (var j = 0; j < currentCourses.length; j++) {
-            console.log(currentCourses[j]);
+            // console.log(currentCourses[j]);
             let currentCourse = await Course.findOne({$and: [{school: "5c36528ba2301f5fd713cdb1"}, {teacher: currentCourses[j].teacher}, {block: currentCourses[j].block}]});
             console.log(currentCourse);
             // newCourses.push(currentCourse._id);
