6993ab342f74fc8f4643f23a712c5b640b9f31b5
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-02-07 07:54:35 -0800

    new home page

--- files/index.js
+++ files/index.js
@@ -333,12 +333,14 @@ app.use(async function(req, res, next) {
   try {
     if (validPaths.indexOf(url.parse(req.url).pathname) >= 0 && req.session.userId) {
       let user = await User.findOne({_id : req.session.userId});
-      let date = (new Date()).local();
-      let formatted = moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
-      let info = `accessed ${url.parse(req.url).pathname} via method ${req.method}`;
-      let data = {user: user.username, dateString: formatted, info: info};
-      logSchool(user.school, data);
-      next();
+      if (user.username != "AidanEglin") {
+        let date = (new Date()).local();
+        let formatted = moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+        let info = `accessed ${url.parse(req.url).pathname} via method ${req.method}`;
+        let data = {user: user.username, dateString: formatted, info: info};
+        logSchool(user.school, data);
+        next();
+      }
     } else {
       next();
     }
