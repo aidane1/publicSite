304ecc2532f71df6b7a0802100547001df7370fb
Aidan Eglin <aidaneglin@Aidans-MacBook-Pro.local>
2019-03-06 21:52:51 -0800

    fixed user logger

--- files/index.js
+++ files/index.js
@@ -344,7 +344,7 @@ app.use(async function(req, res, next) {
   try {
     if (!req.query.preload && validPaths.indexOf(url.parse(req.url).pathname) >= 0 && req.session.userId) {
       let user = await User.findOne({_id : req.session.userId});
-      if (user.username != "dick") {
+      if (user.username != "AidanEglin") {
         let date = (new Date()).local();
         let formatted = moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
         let info = `accessed ${url.parse(req.url).pathname} via method ${req.method}`;
@@ -374,7 +374,7 @@ function wwwHttpsRedirect(req, res, next) {
     }
 };
 
-// app.use(wwwHttpsRedirect);
+app.use(wwwHttpsRedirect);
 
 let server = app.listen(80, function() {
   console.log("listening for requests");
